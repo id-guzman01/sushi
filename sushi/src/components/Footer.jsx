dc66e80e46c7879ba0627af3d8438b0446e4b649
Daniel Guzmán <danieljk001@yahoo.com>
2023-05-31 19:37:29 -0500

    Registro de usuarios

--- sushi/src/components/Footer.jsx
+++ sushi/src/components/Footer.jsx
@@ -10,27 +10,17 @@ const Footer = () => {
 
   return (
     <>
-      <footer id={StyleFooter.pie_pagina}>
+        <footer id={StyleFooter.pie_pagina}>
         <picture id={StyleFooter.content_img}>
           <img src="./logo.png" alt="Logo del restaurante" />
         </picture>
 
         <section id={StyleFooter.encuentranos}>
           <h2 className={StyleFooter.subtitulo_footer}>Encuentranos</h2>
-          <p className={StyleFooter.encuentranos_footer}>
-            10110 139 St Edmonton
-          </p>
-          <p className={StyleFooter.encuentranos_footer}>+1 5873461285</p>
-          <p className={StyleFooter.encuentranos_footer}>info@zensushi.com</p>
+          <p className={StyleFooter.encuentranos_footer}>10110 139 St Edmonton</p>
+          <p className={StyleFooter.encuentranos_footer}>20330 139 St Edmonton</p>       
         </section>
 
-<<<<<<< HEAD
-            <section id={StyleFooter.encuentranos}>
-                <h2 className={StyleFooter.subtitulo_footer}>Encuentranos</h2>
-                <p className={StyleFooter.encuentranos_footer}>10110 139 St Edmonton</p>
-                <p className={StyleFooter.encuentranos_footer}>20330 139 St Edmonton</p>       
-            </section>
-=======
         <nav id={StyleFooter.redes}>
           <a
             className={StyleFooter.btn_social}
@@ -57,31 +47,7 @@ const Footer = () => {
             <i className="fa-brands fa-twitter"></i>
           </a>
         </nav>
->>>>>>> 925128834f0ebd47a2c7be4ffaed3b4abd3d8955
 
-        <section id={StyleFooter.informacion}>
-          <h2 className={StyleFooter.subtitulo_footer}>
-            Información importante
-          </h2>
-          <details
-            className={StyleFooter.details_footer}
-            onClick={() => {
-              open(1);
-            }}
-            open={openIndex == 1}
-          >
-            <summary className={StyleFooter.summary_footer}>
-              Políticas de privacidad
-            </summary>
-            <p className={StyleFooter.text_details_footer}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
-              exercitationem itaque consequatur, ipsum excepturi esse dicta.
-              Modi sed nihil distinctio laboriosam, totam beatae sequi
-              perferendis, dolor vero fugit laudantium deserunt?
-            </p>
-          </details>
-
-<<<<<<< HEAD
             <section id={StyleFooter.informacion}>
                 <h2 className={StyleFooter.subtitulo_footer}>Información importante</h2>
                 <details className={StyleFooter.details_footer} onClick={(evento) => {
@@ -112,40 +78,9 @@ const Footer = () => {
             </section>
 
             <p id={StyleFooter.derechos}><span id={StyleFooter.signo}>&copy;</span> 2023 - All Rights Reserved By Zen Sushi</p>
-
+      
         </footer>
     </>);
 }
 
 export default Footer;
-=======
-          <details
-            className={StyleFooter.details_footer}
-            onClick={() => {
-              open(2);
-            }}
-            open={openIndex == 2}
-          >
-            <summary className={StyleFooter.summary_footer}>
-              Preguntas más frecuentes
-            </summary>
-            <p className={StyleFooter.text_details_footer}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-              eligendi qui incidunt aspernatur praesentium dicta adipisci? Eius
-              magnam ut ipsam, amet officiis explicabo ipsum sunt. Mollitia
-              veritatis quis voluptate accusantium!
-            </p>
-          </details>
-        </section>
-
-        <p id={StyleFooter.derechos}>
-          <span id={StyleFooter.signo}>&copy;</span> 2023 - All Rights Reserved
-          By Zen Sushi
-        </p>
-      </footer>
-    </>
-  );
-};
-
-export default Footer;
->>>>>>> 925128834f0ebd47a2c7be4ffaed3b4abd3d8955
--- sushi/src/main.jsx
+++ sushi/src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Footer from './components/Footer.jsx'
+import Products from './components/Products.jsx'
+import Cart from './components/Cart.jsx'
+import Register from './components/Register.jsx'
+import RegistroPrvider from './provider/RegistroProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Footer />
+    <RegistroPrvider>
+      <Register />
+      <Cart />
+      <Products />
+      <Footer />
+    </RegistroPrvider>
   </React.StrictMode>,
 )
