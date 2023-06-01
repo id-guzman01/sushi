4f3d78d85ad88e04d458adea3350559a4d3b558d
jhoanfelipe1919@gmail.com <jhoanfelipe1919@gmail.com>
2023-06-01 10:47:01 -0500

    Cards sushi, estilos, responsivo

--- sushi/src/main.jsx
+++ sushi/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Footer from './components/Footer.jsx'
-import Products from './components/Products.jsx'
-import Cart from './components/Cart.jsx'
-import Register from './components/Register.jsx'
-import Navbar from './components/Navbar.jsx'
-import RegistroPrvider from './provider/RegistroProvider.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Footer from "./components/Footer.jsx";
+import Products from "./components/Products.jsx";
+import Cart from "./components/Cart.jsx";
+import Register from "./components/Register.jsx";
+import Navbar from "./components/Navbar.jsx";
+import RegistroPrvider from "./provider/RegistroProvider.jsx";
+import Catalogo_sushi from "./components/Catalogo.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RegistroPrvider>
-    </RegistroPrvider>
-  </React.StrictMode>,
-)
+    <RegistroPrvider></RegistroPrvider>
+  </React.StrictMode>
+);
