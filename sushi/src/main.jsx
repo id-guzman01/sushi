8ab3af69b0b6aacb4678629ffcd8ee5f9334093a
jhoanfelipe1919@gmail.com <jhoanfelipe1919@gmail.com>
2023-06-02 01:59:05 -0500

    Eventos en Navbar

--- sushi/src/main.jsx
+++ sushi/src/main.jsx
@@ -9,30 +9,40 @@ import Register from "./components/Register.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Sushi from "./components/Sushi.jsx";
 import Bebidas from "./components/Bebidas.jsx";
+import Login from "./components/Login.jsx";
 import Postres from "./components/Postres.jsx";
 import RegistroPrvider from "./provider/RegistroProvider.jsx";
 import SushiProvider from "./provider/SushiProvider.jsx";
 import CategoriaProvider from "./provider/CategoriaProvider.jsx";
 import BebidasProvider from "./provider/BebidasProvider.jsx";
 import PostresProvider from "./provider/PostresProvider.jsx";
+import UserProvider from "./provider/UserProvider.jsx";
+import AuthProvider from "./provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SushiProvider>
-      <RegistroPrvider>
-        <CategoriaProvider>
-          <BebidasProvider>
-            <PostresProvider>
-              <Products />
-              <Cart />
-              <Sushi />
-              <Bebidas />
-              <Postres />
-              <Footer />
-            </PostresProvider>
-          </BebidasProvider>
-        </CategoriaProvider>
-      </RegistroPrvider>
-    </SushiProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <UserProvider>
+        <SushiProvider>
+          <RegistroPrvider>
+            <CategoriaProvider>
+              <BebidasProvider>
+                <PostresProvider>
+                  <Navbar />
+                  <Login />
+                  <Register />
+                  <Products />
+                  <Cart />
+                  <Sushi />
+                  <Bebidas />
+                  <Postres />
+                  <Footer />
+                </PostresProvider>
+              </BebidasProvider>
+            </CategoriaProvider>
+          </RegistroPrvider>
+        </SushiProvider>
+      </UserProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
